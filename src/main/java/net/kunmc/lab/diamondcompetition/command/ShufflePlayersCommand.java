7df016fb628c285e132ce814fe6672f912babc03
maru-256 <maru043@docomo.ne.jp>
2021-09-02 01:10:49 +0900

    shuffleplayersの対象をチーム未所属のみに変更

--- src/main/java/net/kunmc/lab/diamondcompetition/command/ShufflePlayersCommand.java
+++ src/main/java/net/kunmc/lab/diamondcompetition/command/ShufflePlayersCommand.java
@@ -5,15 +5,23 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.scoreboard.Team;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ShufflePlayersCommand implements SubCommand {
     @Override
     public void run(CommandSender sender, String[] args) {
+        Team blueTeam = Utils.getBlueTeam();
+        Team redTeam = Utils.getRedTeam();
         List<Player> playerList = new ArrayList<>(Bukkit.getOnlinePlayers());
+        playerList = playerList.stream()
+                .filter(p -> !blueTeam.hasEntry(p.getName()))
+                .filter(p -> !redTeam.hasEntry(p.getName()))
+                .collect(Collectors.toList());
         Collections.shuffle(playerList);
 
         int count = 0;
@@ -26,7 +34,7 @@ public class ShufflePlayersCommand implements SubCommand {
             count++;
         }
 
-        sender.sendMessage(ChatColor.GREEN + "現在オンラインのプレイヤーの所属チームをシャッフルしました.");
+        sender.sendMessage(ChatColor.GREEN + "" + count + "人のプレイヤーをランダムにチームに割り当てました.");
     }
 
     @Override
