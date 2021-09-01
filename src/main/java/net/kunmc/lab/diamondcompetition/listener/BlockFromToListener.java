067e4ad61b4ed2ff7eaa9f137104ed57781bcb6e
maru-256 <maru043@docomo.ne.jp>
2021-09-01 20:31:06 +0900

    水と溶岩が広がってしまう問題を修正

--- src/main/java/net/kunmc/lab/diamondcompetition/listener/BlockFromToListener.java
+++ src/main/java/net/kunmc/lab/diamondcompetition/listener/BlockFromToListener.java
@@ -9,12 +9,13 @@ import org.bukkit.event.block.BlockFromToEvent;
 public class BlockFromToListener implements Listener {
     @EventHandler
     public void OnBlockFromTo(BlockFromToEvent e) {
-        if (Config.preventWaterSpreading && e.getBlock().getType().equals(Material.WATER)) {
+        Material material = e.getBlock().getType();
+        if (Config.preventWaterSpreading && (material.equals(Material.WATER) || material.equals(Material.STATIONARY_WATER))) {
             e.setCancelled(true);
             return;
         }
 
-        if (Config.preventLavaSpreading && e.getBlock().getType().equals(Material.LAVA)) {
+        if (Config.preventLavaSpreading && (material.equals(Material.LAVA) || material.equals(Material.STATIONARY_LAVA))) {
             e.setCancelled(true);
         }
     }
